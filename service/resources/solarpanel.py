d144db5f1fa2a66407ad37e50c0a968a4f80f372
henry.jiang@sfgov.org <henry.jiang@sfgov.org>
2021-11-10 09:36:20 -0800

    updates

--- service/resources/solarpanel.py
+++ service/resources/solarpanel.py
@@ -3,6 +3,7 @@ import os
 import json
 import time
 import logging
+import pathlib
 import requests
 import falcon
 import jsend
@@ -24,12 +25,14 @@ class SolarPanel():
         try:
             data = json.loads(req.bounded_stream.read())
             if data['request']:
+                self.prepare_data(data['request'])
+                print(data['request'])
                 pdf = self.get_pdf(data['request']['data'], req.get_header('TEMPLATE_FILE'))
                 if pdf.content:
                     filename = "generated_pdf_" + str(time.time()) + ".pdf"
                     # stored the generated pdf in a tmp folder, to be removed later
                     temp_file= os.path.dirname(__file__) + "/tmp/" + filename
-                    #writes pdf to a local temp file because sending pdf bytes to a json stream is complicated
+                    #writes pdf to a local temp file because sending pdf bytes to a json stream(encoding) is complicated
                     with open(temp_file, 'wb') as fd:
                         fd.write(pdf.content)
                         fd.close()
@@ -38,7 +41,7 @@ class SolarPanel():
                         # allow access to the generated pdf for email attachment
                         file_url = req.url.replace("solar-panel", "static") + "/" + filename
                         self.send_email(data['request'], emails, file_url, 'staffs')
-                        self.send_email(data['request'], emails, file_url, 'applicants')
+                        #self.send_email(data['request'], emails, file_url, 'applicants')
                 else:
                     raise ValueError(ERROR_PDF)
 
@@ -58,26 +61,26 @@ class SolarPanel():
             resp.body = json.dumps(jsend.error(msg_error))
 
     #pylint: disable=no-self-use,too-many-locals
-    def send_email(self, data, emails, file_url, type):
+    def send_email(self, request, emails, file_url, type):
         """
         send emails applicant and staff
         """
         template = {
-            "url": data["staff_email_template"],
+            "url": request["staff_email_template"],
             "replacements": {
-                "data": data['data']
+                "data": request
             }
         }
-        subject = data['data']["ContractorApplicantName"] + " applied for a solar permit at " + data['data']["projectAddress"]
+        subject = request['data']["ContractorApplicantName"] + " applied for a solar permit at " + request['data']["projectAddress"]
         email_to = emails["staffs"]
         #applicant email
         if type == "applicants":
-            subject = "You applied for a solar permit at " + data['data']["projectAddress"]
+            subject = "You applied for a solar permit at " + request['data']["projectAddress"]
             email_to = emails["applicants"]
             template = {
-                "url": data["applicant_email_template"],
+                "url": request["applicant_email_template"],
                 "replacements": {
-                    "data": data['data']
+                    "data": request['data']
                 }
             }
 
@@ -119,7 +122,49 @@ class SolarPanel():
 
         return result
 
-#pylint: disable=no-self-use,too-many-locals
+    #pylint: disable=no-self-use,too-many-locals
+    def prepare_data(self, request):
+        """
+        Prepare form data for pdf geeneration and email
+        """
+        # update file upload names
+        project_address = request['data']["projectAddress"]
+        fe = pathlib.Path(request['data']["planDrawings"][0]["originalName"]).suffix
+        request['data']["planDrawings"][0]["originalName"] = project_address + "-plans" + fe
+        fe = pathlib.Path(request['data']["dataSheets"][0]["originalName"]).suffix
+        request['data']["dataSheets"][0]["originalName"] = project_address + "-cut" + fe
+        if request['data']['structuralReview']:
+            fe = pathlib.Path(request['data']["structuralReview"][0]["originalName"]).suffix
+            request['data']["structuralReview"][0]["originalName"] = project_address + "-str" + fe
+
+        # flatten lists into string
+        request['data']['occupancyClass'] = ", ".join(str(x) for x in request['data']['occupancyClass'])
+
+        # flatten multiple checkboxes
+        cb = []
+        for k,v in request['data']['LicenseClass'].items():
+            if v:
+                if k == "Other":
+                    cb.append(request['data']['OtherLicenseClass'])
+                else:
+                    cb.append(k)
+        request['data']['LicenseClass'] = ", ".join(cb)
+
+        # logic for applicant name/email
+        if request['data']['whatIsYourRoleInThisProject'] == 'propertyOwner':
+            email = request['data']['ownersEmailAddress']
+            name = request['data']['OwnerName']
+        elif request['data']['whatIsYourRoleInThisProject'] == 'contractor':
+            email = request['data']['ApplicantEmailAddress']
+            name = request['data']['ContractorApplicantName']
+        else:
+            email = request['data']['yourEmail']
+            name = request['data']['yourName']
+
+        request['emails']['applicants'][0]['email'] = email
+        request['emails']['applicants'][0]['name'] = name
+
+    #pylint: disable=no-self-use,too-many-locals
     def get_emails(self, emails):
         """
         get email information from payload
